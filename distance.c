9db6879a05931c35c7ad649b5a433f9e37799fb5
chenyiyang <2775018322@qq.com>
2025-03-11 20:53:36 +0800

    debug distance.c

--- distance.c
+++ distance.c
@@ -5,19 +5,25 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <sys/shm.h>
+#include <limits.h>
 
 volatile uint8_t *trace_bits = NULL;
 
 void setup_shm() {
     char *shm_env = getenv("__AFL_SHM_ID");
-    if (shm_env) {
+    if (shm_env && !trace_bits) {
         int shm_id = atoi(shm_env);
         trace_bits = (uint8_t *) shmat(shm_id, NULL, 0);
         if (trace_bits == (void *) -1) {
             perror("shmat");
             exit(1);
         }
+        int *shm_distance_orig = (int *)(trace_bits + MAP_SIZE + 16);
+        *shm_distance_orig = INT_MAX;
+        uint64_t *reach_tag_orig = (uint64_t *)(trace_bits + MAP_SIZE + 24);
+        *reach_tag_orig = 0;
     }
+    
 }
 
 void distance_instrument(int distance) {
@@ -26,7 +32,8 @@ void distance_instrument(int distance) {
         abort();
     }
     int *shm_distance = (int *)(trace_bits + MAP_SIZE + 16);
-    *shm_distance = distance;
+    if (distance < *shm_distance)
+        *shm_distance = distance;
     uint64_t *reach_tag = (uint64_t *)(trace_bits + MAP_SIZE + 24);
     *reach_tag = 1;
 }
